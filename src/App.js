d3d78a9c120b499ef8f3349c28e47192e4d3d1ca
joseph-caldwell <caldwelljoey36@gmail.com>
2021-11-04 23:43:27 -0500

    commit css changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       
       <Navbar />
+      <Header />
     </div>
   );
 }
