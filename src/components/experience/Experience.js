e48789c327b986603f572a7d9f9563cfb5dff391
joseph-caldwell <caldwelljoey36@gmail.com>
2021-11-14 14:47:45 -0600

    fixed timeline

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './experience.css';
 
 const Experience = () => {
   return (
     <div className="experience">
       <div className="d-flex justify-content-center my-5">
-        <h1>experience</h1>
+        <h1>Experience</h1>
       </div>
       <div className="container experience-wrapper">
         <div className="timeline-block timeline-block-right">
